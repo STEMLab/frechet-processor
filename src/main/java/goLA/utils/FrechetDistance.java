b4b5ce04c37c2bf1fc928d1eb7da72fc3b682ea9
dongmin.kim <dongmin.kim@pnu.edu>
2017-07-21 11:12:58 +0900

    update FrechetDistance Decision

--- src/main/java/goLA/utils/FrechetDistance.java
+++ src/main/java/goLA/utils/FrechetDistance.java
@@ -22,42 +22,27 @@ public class FrechetDistance {
         if (EuclideanDistance.distance(p_coordinates.get(p), q_coordinates.get(q)) > dist || EuclideanDistance.distance(p_coordinates.get(0), q_coordinates.get(0)) > dist)
             return false;
 
-        boolean[][] bottom = new boolean[p + 1][q + 1];
-        boolean[][] left = new boolean[p + 1][q + 1];
-
         boolean[][] result_bottom = new boolean[p + 1][q + 1];
         boolean[][] result_left = new boolean[p + 1][q + 1];
 
         for (int i = 0; i < p; i++) {
-            for (int j = 0; j < q; j++) {
-                //i && (j,j+1)
-                left[i][j] = (dist >= EuclideanDistance.pointAndLine(p_coordinates.get(i), q_coordinates.get(j), q_coordinates.get(j + 1)));
-
-                //j && (i,i+1)
-                bottom[i][j] = (dist >= EuclideanDistance.pointAndLine(q_coordinates.get(j), p_coordinates.get(i), p_coordinates.get(i + 1)));
-            }
-        }
-
-        for (int i = 0; i < p; i++) {
-            result_bottom[i][0] = bottom[i][0];
-            bottom[i][q] = (dist >= EuclideanDistance.pointAndLine(q_coordinates.get(q), p_coordinates.get(i), p_coordinates.get(i + 1)));
+            result_bottom[i][0] = (dist >= EuclideanDistance.pointAndLine(q_coordinates.get(0), p_coordinates.get(i), p_coordinates.get(i + 1)));
         }
 
         for (int j = 0; j < q; j++) {
-            result_left[0][j] = left[0][j];
-            left[p][j] = (dist >= EuclideanDistance.pointAndLine(p_coordinates.get(p), q_coordinates.get(j), q_coordinates.get(j + 1)));
+            result_left[0][j] = (dist >= EuclideanDistance.pointAndLine(p_coordinates.get(0), q_coordinates.get(j), q_coordinates.get(j + 1)));
         }
 
         for (int i = 0; i < p; i++) {
             boolean sw = false;
             for (int j = 0; j < q; j++) {
-                if (!result_left[i][j] && !result_bottom[i][j]) {
+                if (!result_left[i][j] && !result_bottom[i][j]){
                     result_left[i + 1][j] = false;
                     result_bottom[i][j + 1] = false;
                 } else {
                     sw = true;
-                    result_bottom[i][j + 1] = bottom[i][j + 1];
-                    result_left[i + 1][j] = left[i + 1][j];
+                    result_bottom[i][j + 1] = (dist >= EuclideanDistance.pointAndLine(q_coordinates.get(j+1), p_coordinates.get(i), p_coordinates.get(i + 1)));
+                    result_left[i + 1][j] = (dist >= EuclideanDistance.pointAndLine(p_coordinates.get(i+1), q_coordinates.get(j), q_coordinates.get(j + 1)));
                 }
             }
             if (!sw) return false;
@@ -68,4 +53,5 @@ public class FrechetDistance {
         }
         return false;
     }
+
 }
