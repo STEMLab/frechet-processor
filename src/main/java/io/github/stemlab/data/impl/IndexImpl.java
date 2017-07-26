b2c6d2a9b00697587fd55182bbb3c14daa31c4f6
cocoslime <dongmin.kim@pusan.ac.kr>
2017-07-26 16:18:38 +0900

    extract DecisionMaker

--- src/main/java/io/github/stemlab/data/impl/IndexImpl.java
+++ src/main/java/io/github/stemlab/data/impl/IndexImpl.java
@@ -2,16 +2,14 @@ package io.github.stemlab.data.impl;
 
 import de.lmu.ifi.dbs.elki.database.ids.DoubleDBIDList;
 import de.lmu.ifi.dbs.elki.database.ids.DoubleDBIDListIter;
+import io.github.stemlab.decision.DecisionMaker;
 import io.github.stemlab.model.Query;
 import io.github.stemlab.data.Index;
 import io.github.stemlab.data.elki.ELKIRStarTree;
 import io.github.stemlab.model.Coordinate;
-import io.github.stemlab.model.Query;
 import io.github.stemlab.model.Trajectory;
-import io.github.stemlab.utils.DiscreteFrechetDistance;
 import io.github.stemlab.utils.DouglasPeucker;
 import io.github.stemlab.utils.EuclideanDistance;
-import io.github.stemlab.utils.FrechetDistance;
 
 import java.util.HashSet;
 import java.util.LinkedHashSet;
@@ -21,6 +19,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class IndexImpl implements Index {
 
+    private final DecisionMaker decisionMaker = new DecisionMaker();
     private ELKIRStarTree rStarTree;
     private ConcurrentHashMap<String, Trajectory> holder;
     private int size;
@@ -65,19 +64,16 @@ public class IndexImpl implements Index {
             Trajectory trajectory = this.holder.get(rStarTree.getRecordName(x));
             Coordinate last = trajectory.getCoordinates().get(trajectory.getCoordinates().size() - 1);
             if (EuclideanDistance.distance(last, end) <= dist) {
-                if (isFiltered(simple, trajectory, q_dist, maxEpsilon)) {
-                    if (isResult(query, trajectory, maxEpsilon)) {
-                        resultSet.add(trajectory.getName());
-                    }
-                    else
-                        if (decision(query, trajectory)) {
-                            resultSet.add(trajectory.getName());
-                        }
+                if (decisionMaker.decisionIsInResult(query, simple, q_dist, maxEpsilon, trajectory)){
+                    resultSet.add(trajectory.getName());
                 }
             }
-
         }
         return resultSet;
+    }
+
+    private boolean decisionIsInResult(Query query, Trajectory simple, double q_dist, double maxEpsilon, Trajectory trajectory) {
+        return decisionMaker.decisionIsInResult(query, simple, q_dist, maxEpsilon, trajectory);
     }
 
     @Override
@@ -86,25 +82,15 @@ public class IndexImpl implements Index {
     }
 
     private boolean isResult(Query q, Trajectory tr, double q_max_E) {
-        if (q.getDistance() - q_max_E >= 0){
-            return DiscreteFrechetDistance.decisionDP(q.getTrajectory(),
-                    tr.getSimplified(),q.getDistance() - q_max_E);
-        }
-        else
-            return false;
+        return decisionMaker.isResult(q, tr, q_max_E);
     }
 
     private boolean isFiltered(Trajectory simple, Trajectory tr, double dist, double q_max_E) {
-        return FrechetDistance.decisionDP(simple, DouglasPeucker.getReduced(tr, q_max_E),
-                dist + q_max_E * 2);
+        return decisionMaker.isFiltered(simple, tr, dist, q_max_E);
     }
 
 
-    private boolean decision(Query q, Trajectory t) {
-        if (DiscreteFrechetDistance.decisionDP(q.getTrajectory(), t, q.getDistance())){
-            return true;
-        }
-        else
-            return FrechetDistance.decisionDP(q.getTrajectory(), t, q.getDistance());
+    private boolean isTrajectoryInQueryRange(Query q, Trajectory t) {
+        return decisionMaker.isTrajectoryInQueryRange(q, t);
     }
 }
