b01a0f7a31f113adc07ed1fb48547562ebd5b95c
dongmin.kim <dongmin.kim@pnu.edu>
2017-07-28 22:07:25 +0900

    Trajectory and Coordinate

--- src/main/java/io/github/stemlab/model/Trajectory.java
+++ src/main/java/io/github/stemlab/model/Trajectory.java
@@ -3,16 +3,11 @@ package io.github.stemlab.model;
 import java.util.List;
 
 public class Trajectory {
-
-    private boolean isResult;
-    private Double maxEpsilon;
     private Trajectory simplified;
     private String name;
     private List<Coordinate> coordinates;
 
     public Trajectory() {
-        this.isResult = false;
-        this.maxEpsilon = null;
         this.simplified = null;
     }
 
@@ -30,22 +25,6 @@ public class Trajectory {
 
     public void setCoordinates(List<Coordinate> coordinates) {
         this.coordinates = coordinates;
-    }
-
-    public boolean isResult() {
-        return this.isResult;
-    }
-
-    public void setResult(boolean result) {
-        this.isResult = result;
-    }
-
-    public Double getMaxEpsilon() {
-        return maxEpsilon;
-    }
-
-    public void setMaxEpsilon(Double maxEpsilon) {
-        this.maxEpsilon = maxEpsilon;
     }
 
     public Trajectory getSimplified() {
