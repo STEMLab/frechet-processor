80e0a22900264400b2f486f402111821b68f5675
cocoslime <dongmin.kim@pusan.ac.kr>
2017-07-28 11:29:01 +0900

    resolve

--- src/main/java/io/github/stemlab/utils/SimplificationFrechetDecision.java
+++ src/main/java/io/github/stemlab/utils/SimplificationFrechetDecision.java
@@ -2,17 +2,15 @@ package io.github.stemlab.utils;
 
 import io.github.stemlab.model.Query;
 import io.github.stemlab.model.Trajectory;
-import io.github.stemlab.utils.DiscreteFrechetDistance;
-import io.github.stemlab.utils.DouglasPeucker;
-import io.github.stemlab.utils.FrechetDistance;
 
 public class SimplificationFrechetDecision {
 
     /**
      * Decide whether trajectory is in query range.
      */
-    public static boolean decisionIsInResult(Query query, double q_dist, double maxEpsilon, Trajectory trajectory) {
+    public static boolean decisionIsInResult(Query query, double maxEpsilon, Trajectory trajectory) {
         Trajectory simple = query.getTrajectory().getSimplified();
+        double q_dist = query.getDistance();
         if (isFiltered(simple, trajectory, q_dist, maxEpsilon)) {
             if (isResult(query, trajectory, maxEpsilon)) {
                 return true;
@@ -24,21 +22,31 @@ public class SimplificationFrechetDecision {
     }
 
     /**
-     * Decide whether trajectory is definetly
+     * Decide whether trajectory is sure in result by using simplification.
      */
-    public static boolean isResult(Query q, Trajectory tr, double q_max_E) {
-        if (q.getDistance() - q_max_E >= 0) {
-            return DiscreteFrechetDistance.decisionDP(q.getTrajectory(),
-                    tr.getSimplified(), q.getDistance() - q_max_E);
+    public static boolean isResult(Query query, Trajectory trajectory, double q_max_E) {
+        if (query.getDistance() - q_max_E >= 0) {
+            return DiscreteFrechetDistance.decisionDP(query.getTrajectory(),
+                    trajectory.getSimplified(), query.getDistance() - q_max_E);
         } else
             return false;
     }
 
-    public static boolean isFiltered(Trajectory simple, Trajectory tr, double dist, double q_max_E) {
-        return FrechetDistance.decisionDP(simple, DouglasPeucker.getReduced(tr, q_max_E),
+    /**
+     * Decide whether trajectory is sure in out of result.
+     *
+     */
+    public static boolean isFiltered(Trajectory simple, Trajectory trajectory, double dist, double q_max_E) {
+        return FrechetDistance.decisionDP(simple, DouglasPeucker.getReduced(trajectory, q_max_E),
                 dist + q_max_E * 2);
     }
 
+    /**
+     * First, decide whether discrete Frechet Distance is lower than parameter, if not check real Frechet Distance.
+     * @param q
+     * @param t
+     * @return
+     */
     public static boolean isTrajectoryInQueryRange(Query q, Trajectory t) {
         if (DiscreteFrechetDistance.decisionDP(q.getTrajectory(), t, q.getDistance())) {
             return true;
