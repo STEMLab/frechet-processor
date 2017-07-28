55d8cbf95b0260d62a36148044ac8051a7dc8fc7
dongmin.kim <dongmin.kim@pnu.edu>
2017-07-28 21:18:00 +0900

    make Simplification of query only once.

--- src/main/java/io/github/stemlab/utils/StraightSimpleFrechetDecision.java
+++ src/main/java/io/github/stemlab/utils/StraightSimpleFrechetDecision.java
@@ -8,7 +8,7 @@ import io.github.stemlab.model.Trajectory;
  */
 public class StraightSimpleFrechetDecision {
     public static boolean decisionIsInResult(Query query, Trajectory trajectory) {
-        Trajectory simplifiedQuery = StraightForward.getReduced(query.getTrajectory(), query.getDistance());
+        Trajectory simplifiedQuery = query.getTrajectory().getSimplified();
         Trajectory simplifiedTrajectory = StraightForward.getReduced(trajectory, query.getDistance());
         if (isFiltered(simplifiedQuery, simplifiedTrajectory, query.getDistance())) { // Decide whether simple_trajectory is sure in out of result.
             if (isResult(simplifiedQuery, trajectory, query.getDistance())) { // Decide whether trajectory is sure in result by using simplification.
